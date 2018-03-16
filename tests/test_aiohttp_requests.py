23de9133a0826d69176488d7a418fdc51b7547ef
Max Zheng <mzheng@confluent.io>
2018-03-16 16:08:16 -0700

    Add test

--- tests/test_aiohttp_requests.py
+++ tests/test_aiohttp_requests.py
@@ -1,2 +1,19 @@
-def test_aiohttp_requests():
-    """ Test is code's best friend. ^_^ """
+from aiohttp_requests import requests
+from aioresponses import aioresponses
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_aiohttp_requests():
+    test_url = 'http://dummy-url'
+    test_payload = {'hello': 'world'}
+
+    with aioresponses() as mocked:
+        mocked.get(test_url, payload=test_payload)
+
+        response = await requests.get(test_url)
+        json = await response.json()
+
+        assert test_payload == json
+
+    requests.close()  # Normally called on destroy
