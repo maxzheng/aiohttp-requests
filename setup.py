74baf08ecd5a6665d505034cba7161a19de5597a
Max Zheng <max@metabase.com>
2024-05-11 12:48:54 -0700

    Publish version 0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='aiohttp-requests',
-    version='0.2.4',
+    version='0.2.5',
 
     author='Max Zheng',
     author_email='maxzheng.os @t gmail.com',
